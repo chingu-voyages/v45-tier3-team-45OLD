ef71b4077ee02ada7adef176daf85c3aef5c75b6
yi-lin-1234 <92185695+yi-lin-1234@users.noreply.github.com>
2023-08-21 17:04:59 -0400

    init

--- firebase_config.js
+++ firebase_config.js
@@ -1,17 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import.meta.env;
+
+const apiKey = import.meta.env.VITE_apiKey;
+const authDomain = import.meta.env.VITE_authDomain;
+const projectId = import.meta.env.VITE_projectId;
+const storageBucket = import.meta.env.VITE_storageBucket;
+const messagingSenderId = import.meta.env.VITE_messagingSenderId;
+const appId = import.meta.env.VITE_appId;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkQ0FKHEPhS_IASkMEV8x0EUrX7gr_lQo",
-  authDomain: "fir-auth-8b3cc.firebaseapp.com",
-  projectId: "fir-auth-8b3cc",
-  storageBucket: "fir-auth-8b3cc.appspot.com",
-  messagingSenderId: "1021796458065",
-  appId: "1:1021796458065:web:44f720ee33a780d586e270",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
