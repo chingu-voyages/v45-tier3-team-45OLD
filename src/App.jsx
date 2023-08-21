98dec9f1067f86d34614db22acbe1a8e86ad8b16
yi-lin-1234 <92185695+yi-lin-1234@users.noreply.github.com>
2023-08-21 16:41:04 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 
-import Landing from "./views/landing";
-import Login from "./views/login";
-import Signup from "./views/signup";
+import Landing from "./views/Landing";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Dashboard from "./views/Dashboard";
+import PrivateRoutes from "./views/PrivateRoutes";
+import AllPosts from "./views/AllPosts";
+import Create from "./views/Create";
 
-export default function App() {
+function App() {
   return (
     <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="all-posts" element={<AllPosts />} />
+          <Route path="create" element={<Create />} />
+        </Route>
+      </Route>
     </Routes>
   );
 }
+
+export default App;
---
+++ src/views/PrivateRoutes.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase_config";
+
+function PrivateRoutes() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setIsLoading(false);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return user ? <Outlet /> : <Navigate to="/" />;
+}
+
+export default PrivateRoutes;
